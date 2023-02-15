77b1cca1436a2675c313a964281b0bf51b476b60
Fausto Ramirez <ramirezmedia@gmail.com>
2023-02-15 18:32:09 -0500

    commit tree b715a0d054b199c436f65a769b1b6920a984bd36

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,11 +6,33 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+
+    const newObj = [...foods, newFood];
+    setFoods(newObj);
+  }
+
+  function handleClickDelete(id){
+    const newObj = foods.filter( function(list){
+      if(list.id !== id){
+        return list;
+      }
+    })
+    setFoods(newObj);
+  }
+
+  function handleClickHeat(id){
+    const newObj = foods.filter( function(list){
+      if(list.id == id){
+        list.heatLevel++;
+        return list;
+      } else 
+      return list;
+    })
+    setFoods(newObj);
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={ ()=> handleClickHeat(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
